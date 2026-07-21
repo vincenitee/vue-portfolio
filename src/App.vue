fd7f1f8d3d96dc046aee6bed5161a86484eaf6ce
VINCENT-MIS <vincentbolinget@gmail.com>
2026-07-22 07:12:11 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Hero from "./components/Hero.vue";
+import Sidebar from "./components/Sidebar.vue";
+</script>
+
+<template>
+  <div class="dot-grid min-h-screen flex">
+    <Sidebar />
+    <main class="flex-1 px-8 md:px-16 py-12">
+      <Hero />
+    </main>
+  </div>
+</template>
+
+<style></style>
