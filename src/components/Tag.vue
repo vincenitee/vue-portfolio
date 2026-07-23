22102904148c8c576440e4ac471ddf4f3300241e
VINCENT-MIS <vincentbolinget@gmail.com>
2026-07-23 11:33:10 +0800

    feat: add Tags for the tech stack

---
+++ src/components/Tag.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: Object,
+    default: null,
+  }
+});
+</script>
+
+<template>
+  <span
+    class="font-mono text-[12px] uppercase border border-border px-2 py-1 text-text-primary rounded-sm inline-flex items-center gap-2"
+  >
+    <component :is="icon" v-if="icon" class="w-4 h-4" />
+    {{ title }}
+  </span>
+</template>
+
+<style scoped></style>
